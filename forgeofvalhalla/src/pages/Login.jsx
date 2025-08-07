4913444d978a5a193f465fecb1efb2ee1f76690f
Mstrdstrctn7 <tazriker212@gmail.com>
2025-08-07 02:01:58 -0500

    Create Login.jsx

---
+++ forgeofvalhalla/src/pages/Login.jsx
@@ -0,0 +1,8 @@
+export default function Login() {
+  return (
+    <div style={{ textAlign: 'center', color: 'white', padding: '2rem' }}>
+      <h2>Login to Forge of Valhalla</h2>
+      {/* Add Supabase auth here later */}
+    </div>
+  )
+}
