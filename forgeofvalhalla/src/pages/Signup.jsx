d1462a02298c7500c8861bdfa3b77f29ff0c1829
Mstrdstrctn7 <tazriker212@gmail.com>
2025-08-07 02:02:39 -0500

    Create Signup.jsx

---
+++ forgeofvalhalla/src/pages/Signup.jsx
@@ -0,0 +1,8 @@
+export default function Signup() {
+  return (
+    <div style={{ textAlign: 'center', color: 'white', padding: '2rem' }}>
+      <h2>Create your legend</h2>
+      {/* Add Supabase signup here later */}
+    </div>
+  )
+}
