341a3fa86a1c1c4c0e56712a4302ccab25c2b3a3
Mstrdstrctn7 <tazriker212@gmail.com>
2025-08-07 03:00:45 -0500

    Update Signup.jsx

--- forgeofvalhalla/src/pages/Signup.jsx
+++ forgeofvalhalla/src/pages/Signup.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { supabase } from '../supabaseClient'
+import { supabase } from '/src/supabaseClient'
 
 export default function Signup() {
   const [email, setEmail] = useState('')
